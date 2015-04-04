4416cb4a33cba911f116ef913e3b5e5d83509adf
dylanmei <dylanmei@gmail.com>
2015-04-04 15:19:05 -0700

    Use elevated powershell commands

--- scripts/winrm.bat
+++ scripts/winrm.bat
@@ -6,7 +6,7 @@ cmd.exe /c net stop winrm
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm quickconfig -transport:http
 cmd.exe /c winrm set winrm/config @{MaxTimeoutms="1800000"}
-cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="1024"}
+cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="2048"}
 cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
 cmd.exe /c winrm set winrm/config/client/auth @{Basic="true"}
